b02a3ce00175db439e6e222aafe17b2db97fc288
DockGoRewin <christiankorompis2003@gmail.com>
2026-05-27 06:20:42 +0800

    change api/download

--- server.js
+++ server.js
@@ -26,7 +26,28 @@ app.get("/api/download", async (req, res) => {
         });
     }
 
-    res.redirect(url);
+    try {
+        const response = await axios({
+            method: "GET",
+            url: url,
+            responseType: "stream",
+            headers: {
+                "User-Agent": "AVDML_2.1.242.52-net4_ANDROID,unknown,MDLTaskPreload",
+                "Icy-MetaData": "1",
+                "Connection": "keep-alive"
+            }
+        });
+
+        res.setHeader("Content-Type", "video/mp4");
+        response.data.pipe(res);
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Gagal fetch video",
+            error: err.message
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
