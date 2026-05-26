3404079dc1233159ed5d08f1b3398c29dd6cee03
DockGoRewin <christiankorompis2003@gmail.com>
2026-05-27 03:54:46 +0800

    new file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API hidup");
+});
+
+app.post("/api/download", (req, res) => {
+
+    const { url } = req.body;
+
+    res.json({
+        success: true,
+        url
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running");
+});
