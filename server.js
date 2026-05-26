a47aba4f0bb7609767dab107cfe7c2b5041536ea
DockGoRewin <christiankorompis2003@gmail.com>
2026-05-27 06:06:40 +0800

    add api/downloader

--- server.js
+++ server.js
@@ -9,17 +9,6 @@ app.get("/", (req, res) => {
     res.send("API hidup");
 });
 
-app.post("/api/download", (req, res) => {
-
-    const { url } = req.body;
-
-    res.json({
-        success: true,
-        url
-    });
-});
-
-
 app.get("/api", (req, res) => {
   res.json({
     success: true,
@@ -27,14 +16,18 @@ app.get("/api", (req, res) => {
   })
 })
 
-app.get("/api/download", (req, res) => {
-  const url = req.query.url
+app.get("/api/download", async (req, res) => {
+    const { url } = req.query;
 
-  res.json({
-    success: true,
-    video: url
-  })
-})
+    if (!url || !url.includes("melolostatic.com")) {
+        return res.status(400).json({
+            success: false,
+            message: "URL tidak valid"
+        });
+    }
+
+    res.redirect(url);
+});
 
 const PORT = process.env.PORT || 3000;
 
